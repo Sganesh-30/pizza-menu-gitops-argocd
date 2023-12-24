9232c5873f8154557898f234d8d180fddb09fd15
bhanu prakash <r19cs056@cit.reva.edu.in>
2023-12-24 17:41:27 +0530

    feat: add react and react dom in index file

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDom from "react-dom/client";
+
+function App() {
+  return <h1>Hello React!</h1>;
+}
+
+//React version 18 
+const root = ReactDom.createRoot(document.getElementById("root"));
+root.render(<App />);
